b44bdd8e3e0a250b815ddcf504a8bd2c04235e1f
roman <roman.pavlenko.job@gmail.com>
2024-08-28 14:43:04 +0300

    finished character page

---
+++ src/components/CharacterPage/CharacterPage.tsx
@@ -0,0 +1,145 @@
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../../lib/store"
+import { SingleCharacterFetch } from "../DataFetcher/DataFetcher";
+import { setSelectedCharacter } from "../../lib/features/MiscSlice/MiscSlice";
+import { Character } from "../../utils/types";
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
+
+import './styles.css';
+import { useNavigate } from "react-router-dom";
+export function CharacterPage () {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const currentCharacter = useSelector((state : RootState) => state.misc).selectedCharacter;
+
+  const fetcher = async() => {
+    const currentUrl = window.location.href;
+    let currentId = "";
+    for (let i = currentUrl.length - 1; i > 0; i--) {
+      if (currentUrl[i] !== "/") {
+        currentId += currentUrl[i];
+      } else {
+        break;
+      }
+    }
+    const result = await SingleCharacterFetch(Number(currentId));
+    dispatch(setSelectedCharacter(result))
+  }
+
+  if (currentCharacter === undefined) {
+    fetcher();
+  }
+
+
+  function bioGenerator(character : Character) {
+    switch (character.gender) {
+      case ("Male") : {
+        return (
+          <div className="character__bio">
+            <p>He is {character.species}</p>
+            {
+              (character.origin.name === "unknown") 
+              ? <p>
+                  It's unclear where this {character.species} is from {character.origin.name}
+                </p>
+              : <p>
+                  He is from {character.origin.name}
+                </p>
+            }
+            {(character.status === "unknown") 
+            ? <p>
+              It's unclear what status of this character
+            </p>
+            :<p>
+              Right now he is {character.status}
+            </p>
+            }            
+          </div>
+        )
+      }
+      case ("Female") : {
+        return (
+          <div className="character__bio">
+            <p>She is {character.species}</p>
+            {
+              (character.origin.name === "unknown") 
+              ? <p>
+                  It's unclear where this {character.species} is from {character.origin.name}
+                </p>
+              : <p>
+                  She is from {character.origin.name}
+                </p>
+            }
+            {(character.status === "unknown") 
+            ? <p>
+              It's unclear what status of this character
+            </p>
+            :<p>
+              Right now she is {character.status}
+            </p>
+            }            
+          </div>
+        )
+      }
+      default : {
+        return (
+          <div className="character__bio">
+          <p>It is {character.species}</p>
+          {
+            (character.origin.name === "unknown") 
+            ? <p>
+                It's unclear where this {character.species} is from {character.origin.name}
+              </p>
+            : <p>
+                It is from {character.origin.name}
+              </p>
+          }
+          {(character.status === "unknown") 
+          ? <p>
+            It's unclear what status of this character
+          </p>
+          :<p>
+            Right now it is {character.status}
+          </p>
+          }            
+        </div>
+        )
+      }
+    }
+  }
+
+  
+
+  return(
+    <div className="character">
+      <div className="character__goback">
+        <div
+          className="character__goback--text"
+          onClick={() => {
+            navigate("/")
+            dispatch(setSelectedCharacter(undefined))
+          }}
+        >
+          <KeyboardBackspaceIcon/>
+          Go back
+        </div>
+      </div>
+      <div className="character__container">
+        <h1 className="character__name">
+          {
+            currentCharacter?.name
+          }
+        </h1>
+        <img src={currentCharacter?.image} alt={currentCharacter?.name} />
+        <p className="character__status">
+          {
+            (currentCharacter !== undefined) ? bioGenerator(currentCharacter) : ""
+            
+          }
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,55 @@
+import "./styles.css"
+
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../lib/store";
+import { setSearchValue, setStatus } from "../../lib/features/MiscSlice/MiscSlice";
+import { Status } from "../../utils/enums";
+import AddIcon from '@mui/icons-material/Add';
+
+export function Toolbar() {
+  const searchValue = useSelector((state : RootState) => state.misc).searchValue;
+  const statusValue = useSelector((state: RootState) => state.misc).statusValue;
+  const dispatch = useDispatch();
+  
+
+  return (
+    <div className="toolbar">
+      <Button
+        variant="outlined"
+        sx={{boxSizing: "border-box"}}
+        size="small"
+      >
+        Add new character 
+        <AddIcon />
+      </Button>
+      <TextField
+        id="outlined-basic"
+        label="Character name"
+        variant="outlined"
+        value={searchValue}
+        onChange={(event) => {
+          dispatch(setSearchValue(event.target.value));
+        }}
+      />
+      <FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={statusValue}
+    label="Status"
+    onChange={(event) => {
+      const statusType = event.target.value as Status;
+      dispatch(setStatus(statusType))
+    }}
+  >
+    <MenuItem value={Status.all}>All</MenuItem>
+    <MenuItem value={Status.alive}>Alive</MenuItem>
+    <MenuItem value={Status.dead}>Dead</MenuItem>
+    <MenuItem value={Status.unknown}>Unknown</MenuItem>
+  </Select>
+</FormControl>
+    </div>
+  )
+}
